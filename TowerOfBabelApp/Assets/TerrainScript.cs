90d25412a2bb0d406e3569c0cf524087abbf0a0e
Ruslan <Ruslan,Lomaka@Gmail.com>
2021-01-27 13:22:15 +0200

    gitignore update

---
+++ TowerOfBabelApp/Assets/TerrainScript.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TerrainScript : MonoBehaviour
+{
+    // Disable the behaviour when it becomes invisible...
+    void OnBecameInvisible()
+    {
+        enabled = false;
+    }
+
+    // ...and enable it again when it becomes visible.
+    void OnBecameVisible()
+    {
+        enabled = true;
+    }
+}
