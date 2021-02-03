75d10c4ce462fb5e4c80bd56c72ca2473612807c
Konstantin Zhirnov <me.kashey@gmail.com>
2021-02-03 12:54:06 +0200

    fixed update logic for birds

--- TowerOfBabelApp/Assets/ProjectAssets/Scripts/BirdScript.cs
+++ TowerOfBabelApp/Assets/ProjectAssets/Scripts/BirdScript.cs
@@ -8,12 +8,31 @@ public class BirdScript : MonoBehaviour
     [SerializeField] private AudioClip[] _audioClips;
 
     // Update is called once per frame
-    private void Update()
-    {
-        if(_audioSource.isPlaying) return;
+    //private void Update()
+    //{
+    //    if(_audioSource.isPlaying) return;
 
-        var randomAudioIndex = UnityEngine.Random.Range(0, _audioClips.Length - 1);
-        _audioSource.clip = _audioClips[randomAudioIndex];
-        _audioSource.Play();
+    //    var randomAudioIndex = UnityEngine.Random.Range(0, _audioClips.Length - 1);
+    //    _audioSource.clip = _audioClips[randomAudioIndex];
+    //    _audioSource.Play();
+    //}
+
+    private void Start()
+    {
+        StartCoroutine(Play());
+    }
+
+    IEnumerator Play()
+    {
+        while(true)
+        {
+            yield return new WaitWhile(() => _audioSource.isPlaying);
+            var delay = Random.Range(.5f, 5f);
+            yield return new WaitForSeconds(delay);
+
+            var randomAudioIndex = UnityEngine.Random.Range(0, _audioClips.Length - 1);
+            _audioSource.clip = _audioClips[randomAudioIndex];
+            _audioSource.Play();
+        }
     }
 }
